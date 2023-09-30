7b12d0f0ad1e9fab04e7d06d4db491b2102b1693
s18774 <s18774@pjwstk.edu.pl>
2023-09-30 15:06:56 +0200

    first timer xD2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("halo?");
+        System.out.println("halo?4343");
     }
 }
---
+++ src/RandomClass.java
@@ -0,0 +1,4 @@
+public class RandomClass {
+    int twa= 21;
+    long taa=12;
+}
